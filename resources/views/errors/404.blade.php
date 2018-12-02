18a17c10308da4a3023d5fbc9146d7cf80f50a7c
Unknown <atabek.ucuncu@stu.bahcesehir.edu.tr>
2018-12-02 15:55:07 +0300

    Error sayfası redirect düzenlemesi

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,5 +2,5 @@
 	<div style="font-size: 50px;">BU MODÜL HENÜZ HAZIR DEĞİL</div>
 	<h4>Evet erör sayfalarım da hazır değil</h4>
 	<h5>-.-</h5>
-	<a href="{{ url('/') }}">Şurdan sisteme geri dönebilirsin</a>
+	<a href="/matchup/list/0">Şurdan sisteme geri dönebilirsin</a>
 </div>
