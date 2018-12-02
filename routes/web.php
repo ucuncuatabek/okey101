cdfd5e3fc438fae29c9bc3dc1a4e4a25af9f773a
Unknown <atabek.ucuncu@stu.bahcesehir.edu.tr>
2018-12-02 17:56:33 +0300

    Yorum ve düzenleme

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,16 @@ Route::get('/matchup', function () {
 	return redirect('/matchup/createPaired');
 });
 
-Route::get('/matchup/list/{status}', 'MatchupController@list');
 
+//Burada url den php fonksiyonuna yönlendirme yapıyoruz
+
+Route::get('/matchup/list/{status}', 'MatchupController@list');
 Route::get('/matchup/createPaired', 'MatchupController@createPaired');
-// Route::get('/matchup/createUnpaired', 'MatchupController@createUnpaired');  //dont have the option yet
-Route::post('/matchup/store', 'MatchupController@store');
 Route::get('/matchup/edit/{id}', 'MatchupController@edit');
 Route::get('/matchup/view/{id}', 'MatchupController@view');
 Route::get('/matchup/show/{id}', 'MatchupController@show');
 Route::get('/matchup/endMatch/{id}', 'MatchupController@endMatch');
+
+Route::post('/matchup/store', 'MatchupController@store');
 Route::post('/matchup/addPoint', 'MatchupController@addPoint');
 Route::post('/matchup/editPoint', 'MatchupController@editPoint');
