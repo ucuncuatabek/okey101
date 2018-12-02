ad54aef27eae85334a0812f0643d67c6094bb7d7
Unknown <atabek.ucuncu@stu.bahcesehir.edu.tr>
2018-12-02 16:58:04 +0300

    Error düzenlemesi

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,6 @@
 <div style="text-align: center;">
 	<div style="font-size: 50px;">BU MODÜL HENÜZ HAZIR DEĞİL</div>
-	<h4>Evet erör sayfalarım da hazır değil</h4>
+	<h4>Yoksa katakulli mi yapmaya çalıştın?!</h4>
 	<h5>-.-</h5>
 	<a href="/matchup/list/0">Şurdan sisteme geri dönebilirsin</a>
 </div>
